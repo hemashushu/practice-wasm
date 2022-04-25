950441764bb0e3d64e116bd722206c53c5f0c63a
Hippo Spark (Arch) <hippospark@gmail.com>
2022-04-25 09:39:25 +0800

    准备开始写 binary parser.

--- crates/engine/src/binary_parser.rs
+++ crates/engine/src/binary_parser.rs
@@ -12,10 +12,21 @@ pub fn parse(source: &[u8]) -> Result<Module, ParseError> {
 
 pub enum ParseError {
     Something(&'static str),
+
+    /// 不支持的操作
+    /// 比如读取索引值为非 0 的内存块或者表
+    Unsupported,
+
+    /// leb128 编码或者 UTF-8 编码错误
     DecodingError,
+
+    /// 未预料的结束，即预期的内容不完整
+    /// 比如解析一个函数时，尚未到达末尾源文件就已经到末尾了。
     UnexpectedEnd,
 }
 
+/// 读取一个字节的内容
+/// 返回一个 u8 整数
 fn read_byte(source: &[u8]) -> Result<(u8, &[u8]), ParseError> {
     match source.split_first() {
         Some((first, rest)) => Ok((*first, rest)),
@@ -95,7 +106,8 @@ fn read_f64(source: &[u8]) -> Result<(f64, &[u8]), ParseError> {
     Ok((f64::from_le_bytes(bytes), remains))
 }
 
-/// length:u32 + u32{*}
+/// 读取如下结构的 u32 数组
+/// `length:u32 + u32{*}`
 fn read_u32_vec(source: &[u8]) -> Result<(Vec<u32>, &[u8]), ParseError> {
     let mut remains = source;
     let (length, remains) = read_u32(remains)?;
@@ -109,13 +121,16 @@ fn read_u32_vec(source: &[u8]) -> Result<(Vec<u32>, &[u8]), ParseError> {
     Ok((list, remains))
 }
 
-/// length:u32 + u8{*}
+/// 读取如下结构的 byte 数组
+/// `length:u32 + u8{*}`
 fn read_byte_vec(source: &[u8]) -> Result<(&[u8], &[u8]), ParseError> {
     let mut remains = source;
     let (length, remains) = read_u32(remains)?;
     read_n_bytes(source, length as usize)
 }
 
+/// 读取如下结构的 byte 数组并以 UTF-8 编码转换为 String
+/// `length:u32 + u8{*}`
 fn read_string(source: &[u8]) -> Result<(String, &[u8]), ParseError> {
     let mut remains = source;
     let (bytes, remains) = read_byte_vec(remains)?;
@@ -129,6 +144,14 @@ fn read_string(source: &[u8]) -> Result<(String, &[u8]), ParseError> {
     }
 }
 
+/// 读取一个字节并断言其值为 0
+/// 用于读取 memory/data 和 table/element 等需要指定目标对象索引值，
+/// 但该索引值只能是 0 的场合
 fn consume_byte_zero(source: &[u8]) -> Result<&[u8], ParseError> {
-    todo!()
+    let (byte, remains) = read_byte(source)?;
+    if (byte != 0) {
+        Err(ParseError::Unsupported)
+    }else {
+        Ok(remains)
+    }
 }
